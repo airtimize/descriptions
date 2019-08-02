0c6bcad2f6c3db710ae03e403483f10105c51b56
jaybau <jafbautista@gmail.com>
2019-08-01 21:00:26 -0700

    Connect server to Cassandra database

--- database/cassandra/cassie_index.js
+++ database/cassandra/cassie_index.js
@@ -1,4 +1,38 @@
 const cassandra = require('cassandra-driver');
-const client = new cassandra.Client({ contactPoints: ['h1', 'h2'], localDataCenter: 'datacenter1', keyspace: 'descriptions' });
+const client = new cassandra.Client({ contactPoints: ['127.0.0.1'], localDataCenter: 'datacenter1', keyspace: 'test' });
 
-module.exports = client;
+client.connect(function (err) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Cassandra connected!');
+  }
+});
+
+const findDesc = (id, callback) => {
+  client.execute(`SELECT * FROM descriptions WHERE listings_id = ${id}`, (err, result) => {
+    if (err) {
+      console.log(err);
+      callback(err);
+    } else {
+      let data = result.rows[0];
+      callback(null, data);
+    }
+  });
+};
+
+const findAmen = (id, callback) => {
+  client.execute(`SELECT * FROM amenities WHERE listings_id = ${id}`, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      callback(null, result.rows);
+    }
+  })
+};
+
+module.exports = {
+  client,
+  findDesc,
+  findAmen
+};
--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 //server
+require('newrelic');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const db = require('../database');
+// const db = require('../database');
+const db = require('../database/cassandra/cassie_index.js');
 const port = 3000;
 const expressStaticGzip =require("express-static-gzip");
 
@@ -16,45 +18,58 @@ app.use('/:listingID', expressStaticGzip('public', {
     }
  }));
 
-app.get('/listing/desc/:listingID',(req,res)=>{
-    var id = req.params.listingID
-    db.findDesc(id,(err,data)=>{
-        if(err){
-            res.status(500).send(err);
-        } else {
-            console.log(data)
-            if (data.length) {
-                res.json(data[0])
-            } else {
-                res.status(500)
-            }
-        }        
-    })
+// app.get('/listing/desc/:listingID',(req,res)=>{
+//     var id = req.params.listingID
+//     db.findDesc(id,(err,data)=>{
+//         if(err){
+//             res.status(500).send(err);
+//         } else {
+//             console.log(data)
+//             if (data.length) {
+//                 res.json(data[0])
+//             } else {
+//                 res.status(500)
+//             }
+//         }        
+//     })
+// })
+// app.get('/listing/amenity/:listingID',(req,res)=>{
+//     var id = req.params.listingID
+//     db.findAmen(id,(err,data)=>{
+//         if(err){
+//             res.status(500).send(err);
+//         } else {
+//             if (data.length) {
+//                 res.json(data[0])
+//             } else {
+//                 res.status(500)
+//             }
+//         }
+//     })
+// })
+
+app.get('/listing/desc/:listingID', (req, res) => {
+  var id = req.params.listingID;
+  db.findDesc(id, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }        
+  })
 })
+
 app.get('/listing/amenity/:listingID',(req,res)=>{
-    var id = req.params.listingID
-    db.findAmen(id,(err,data)=>{
-        if(err){
+    var id = req.params.listingID;
+    db.findAmen(id, (err,data)=>{
+        if (err){
             res.status(500).send(err);
         } else {
-            if (data.length) {
-                res.json(data[0])
-            } else {
-                res.status(500)
-            }
+          res.status(200).send(data);
         }
     })
 })
-
-// NEW CRUD
-
-// app.post('/listing/desc/', (req, res) => {
-    
-// })
-
-// app.post('/listing/amenity/', (req, res) => {
-    
-// })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 module.exports = app;
