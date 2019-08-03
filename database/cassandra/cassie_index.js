4c1ad5eb812a13b5f33965b5834d92d76ae78e5a
jaybau <jafbautista@gmail.com>
2019-08-03 13:03:30 -0700

    Stress test service using New Relic and K6, update seeding scripts

--- database/cassandra/cassie_index.js
+++ database/cassandra/cassie_index.js
@@ -1,5 +1,6 @@
 const cassandra = require('cassandra-driver');
 const client = new cassandra.Client({ contactPoints: ['127.0.0.1'], localDataCenter: 'datacenter1', keyspace: 'test' });
+const Uuid = require('cassandra-driver').types.Uuid;
 
 client.connect(function (err) {
   if (err) {
@@ -25,8 +26,39 @@ const findAmen = (id, callback) => {
   client.execute(`SELECT * FROM amenities WHERE listings_id = ${id}`, (err, result) => {
     if (err) {
       console.log(err);
+      callback(err);
     } else {
       callback(null, result.rows);
+    }
+  })
+};
+
+const addDesc = (id, callback) => {
+  const newId = Uuid.random();
+  client.execute(`INSERT INTO test.test_descs (listings_id, bath_rm_num, bed_num, bed_rm_num, beds, 
+  descriptions_id, general, guest_max, highlights, host_name, host_pic, listing_type, location, 
+  title) VALUES (${id}, 3, 2, 1, '1 bed', ${newId}, 'Expedita libero inventore esse sunt consectetur doloribus iusto. Rem dolores tenetur corporis expedita nihil rem sed nemo quis. Porro ipsa nesciunt. Animi dolores ad quas. Cumque iusto inventore iusto praesentium aut nesciunt accusantium at. Accusantium totam est voluptatem aspernatur quidem recusandae dolor. Qui illo aut labore. Sed sit nulla quisquam unde. Veritatis cumque excepturi. Quas maiores est vel perferendis consequatur dolorem quisquam.', 
+  2, {'additional_info': 'null', 'category': 'basic', 'offers': 'false'}, 'Sally', 'http://lorempic.com/80/80', 'Island', 'Hawaii', 'A lovely place');`, 
+  (err) => {
+    if (err) {
+      console.log(err);
+      callback(err);
+    } else {
+      callback(null, 'New description successfully added!');
+    }
+  });
+};
+
+const addAmen = (id, callback) => {
+  const newAmenId = Uuid.random();
+  client.execute(`INSERT INTO test.test_amen (listings_id, name, additional_info, amenities_id, category) 
+  VALUES (${id}, 'Umbrellas', 'For unexpected rainy weather', ${newAmenId}, 'Essentials');`, 
+  (err) => {
+    if (err) {
+      console.log(err);
+      callback(err);
+    } else {
+      callback(null, 'New amenity successfully added!');
     }
   })
 };
@@ -34,5 +66,7 @@ const findAmen = (id, callback) => {
 module.exports = {
   client,
   findDesc,
-  findAmen
+  findAmen,
+  addDesc,
+  addAmen
 };
--- server/index.js
+++ server/index.js
@@ -3,20 +3,19 @@ require('newrelic');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-// const db = require('../database');
 const db = require('../database/cassandra/cassie_index.js');
 const port = 3000;
-const expressStaticGzip =require("express-static-gzip");
+// const expressStaticGzip = require("express-static-gzip");
 
 app.use(bodyParser.json());
-// app.use('/:listingID',express.static("public"));
-app.use('/:listingID', expressStaticGzip('public', {
-    enableBrotli: true,
-    orderPreference: ['br', 'gz'],
-    setHeaders: function (res, path) {
-       res.setHeader("Cache-Control", "public, max-age=31536000");
-    }
- }));
+app.use('/:listingID',express.static('public'));
+// app.use('/:listingID', expressStaticGzip('public', {
+//     enableBrotli: true,
+//     orderPreference: ['br', 'gz'],
+//     setHeaders: function (res, path) {
+//        res.setHeader("Cache-Control", "public, max-age=31536000");
+//     }
+//  }));
 
 // app.get('/listing/desc/:listingID',(req,res)=>{
 //     var id = req.params.listingID
@@ -33,6 +32,7 @@ app.use('/:listingID', expressStaticGzip('public', {
 //         }        
 //     })
 // })
+
 // app.get('/listing/amenity/:listingID',(req,res)=>{
 //     var id = req.params.listingID
 //     db.findAmen(id,(err,data)=>{
@@ -60,15 +60,39 @@ app.get('/listing/desc/:listingID', (req, res) => {
   })
 })
 
-app.get('/listing/amenity/:listingID',(req,res)=>{
+app.get('/listing/amenity/:listingID',(req, res)=>{
     var id = req.params.listingID;
-    db.findAmen(id, (err,data)=>{
-        if (err){
+    db.findAmen(id, (err,data) => {
+        if (err) {
             res.status(500).send(err);
         } else {
           res.status(200).send(data);
         }
     })
+})
+
+app.post('/listing/desc/:listingID', (req, res) => {
+  var id = req.params.listingID;
+  db.addDesc(id, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      console.log(data);
+      res.status(200).send(data);
+    }        
+  })
+})
+
+app.post('/listing/amenity/:listingID',(req, res) =>{
+  var id = req.params.listingID;
+  db.addAmen(id, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
